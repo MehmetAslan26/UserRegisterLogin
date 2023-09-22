68d4607ef579d63983a5679aa5914c1d2d96aaed
MehmetAslan26 <meas6776@gmail.com>
2023-09-22 11:19:44 +0300

    UserRegisterLogin

---
+++ RegisterLogin/src/main/java/com/example/RegisterLogin/repository/UsersRepository.java
@@ -0,0 +1,15 @@
+package com.example.RegisterLogin.repository;
+
+import com.example.RegisterLogin.model.UsersModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<UsersModel,Integer> {
+
+    Optional<UsersModel> findByLoginAndPassword(String login , String password);
+
+
+
+
+}
